d17d698ed64d8840ab6e85eb0fe37b5dbe2e447f
luckman212 <1992842+luckman212@users.noreply.github.com>
2025-02-25 08:28:11 -0500

    FOUC fix, cherry-picked onto release 1.0.0.9
    See https://github.com/zimbres/SSLTrack/issues/4

---
+++ SSLTrack/Services/ThemeMiddleware.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+public class ThemeMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public ThemeMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        if (context.Request.Cookies.TryGetValue("IsDarkMode", out var value))
+        {
+            context.Items["IsDarkMode"] = bool.TryParse(value, out var isDarkMode) && isDarkMode;
+        }
+        else
+        {
+            context.Items["IsDarkMode"] = false;
+        }
+
+        await _next(context);
+    }
+}
